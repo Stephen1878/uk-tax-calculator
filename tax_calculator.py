dbc38719a729f8b047a40d2d3527bb6d4f9be9ba
Stephen1878 <stephengordon89@hotmail.co.uk>
2022-11-13 21:51:56 +0000

    Logic for Plan 2 SLs & top rate of tax added

--- tax_calculator.py
+++ tax_calculator.py
@@ -8,19 +8,26 @@ Created on Tue Feb  1 18:30:37 2022
 annual_salary = float(input('Enter your annual salary: '))
 pension_contribution = float(input('Enter your pension contribution %: '))
 student_loan_plan_number = float(input('Enter your student loan plan no (0 if no student loan): '))
-test = 1
 
 if annual_salary < 50270:
     PAYE = (annual_salary-12570)*0.2
-elif annual_salary >= 50270:
+elif annual_salary <= 150000:
     PAYE = ((annual_salary-(annual_salary-50270)-12570)*0.2) + ((annual_salary-50270)*0.4)
+elif annual_salary > 150000:
+    PAYE = ((annual_salary-(annual_salary-50270)-12570)*0.2) + ((annual_salary-50270)*0.4) + ((annual_salary-150000)*0.45)
     
 if annual_salary < 50270:
     NI = (annual_salary-9568)*0.12
 elif annual_salary >= 50270:
     NI = (annual_salary-(annual_salary-50270)-9568)*0.12 + ((annual_salary-50270)*0.02)
     
-SL = (annual_salary-(1657*12))*0.09
+if student_loan_plan_number == 0:
+    SL = 0
+elif student_loan_plan_number == 1:
+    SL = (annual_salary-(1682*12))*0.09
+elif student_loan_plan_number == 2:
+    SL = (annual_salary-(2274*12))*0.09
+
 pension = annual_salary * (pension_contribution/100)
 total_deductions = PAYE+NI+SL+pension
 net_salary = annual_salary - total_deductions
